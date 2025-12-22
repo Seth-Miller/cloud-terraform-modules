af35cb446abc820b616e326fb3b8934f9ef87b28
Seth-Miller <sethmiller.sm@gmail.com>
2025-12-22 11:57:03 -0600

    updates

--- jenkins/scripts/test-command.sh
+++ jenkins/scripts/test-command.sh
@@ -1,15 +1,28 @@
 #!/bin/sh
 
 getCredentialCommand='/usr/share/git_credential_helper/git_credential_helper.py get'
+expectedOutput=$'username=testuser\npassword=testpassword'
+
+case "$jkEnvironment" in
+  AWS)
+    sshUser=ec2-user
+    ;;
+  OCI)
+    sshUser=oci-user
+    ;;
+  *)
+    sshUser=user
+    ;;
+esac
 
 # create ssh agent
 eval $(ssh-agent -s)
 # add private key to ssh agent
 terraform -chdir=${1} output -raw private_key_pem | ssh-add -
 # execute command
-sshOut=$(ssh -o StrictHostKeyChecking=no ec2-user@$(terraform -chdir="${1}" output -raw public_ip) $getCredentialCommand)
+sshOut=$(ssh -o StrictHostKeyChecking=no $sshUser@$(terraform -chdir="${1}" output -raw public_ip) $getCredentialCommand)
 # kill ssh agent
 ssh-agent -k
 
 # check output of command
-[[ "$sshOut" != $'username=testuser\npassword=testpassword' ]] && exit 1 || exit 0 
+[[ "$sshOut" == "$expectedOutput" ]] && exit 0 || exit 1 
