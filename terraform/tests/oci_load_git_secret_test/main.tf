dbb31ba35723d4c6d1f485ba872427901085d527
Seth-Miller <sethmiller.sm@gmail.com>
2026-01-06 20:27:59 -0600

    updates

--- terraform/tests/oci_load_git_secret_test/main.tf
+++ terraform/tests/oci_load_git_secret_test/main.tf
@@ -1,14 +1,3 @@
-data "external" "getvault" {
-  program = ["python", "${path.module}/getvault.py"]
-
-  query = {
-    vault_name = "${var.project_name}_git-secret"
-    secret_name = "${var.project_name}_git-secret"
-    key_name = "${var.project_name}_git-secret"
-    working_dir = abspath("${path.root}")
-  }
-}
-
 module "oci_load_git_secret" {
   source = "../../modules/oci_load_git_secret"
 
